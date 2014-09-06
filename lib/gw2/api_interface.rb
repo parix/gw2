b2274b331c2718c2502f20798443e3637f7b207e
Beau Dacious <dacious.beau@gmail.com>
2014-09-06 01:13:43 -0700

    create .get method for api requests

--- lib/gw2/api_interface.rb
+++ lib/gw2/api_interface.rb
@@ -5,5 +5,17 @@ module GW2
   class ApiInterface
     extend HTTPS
     extend JSON
+
+    private
+
+    def self.get(endpoint, query = {})
+      endpoint_path = endpoint_path(endpoint)
+      response = request(endpoint_path, query: query)
+      parse(response.body)
+    end
+
+    def self.endpoint_path(endpoint)
+      "/#{endpoint}.json"
+    end
   end
 end
--- lib/gw2/item.rb
+++ lib/gw2/item.rb
@@ -1,11 +1,11 @@
 module GW2
   class Item < ApiInterface
     def self.all
-      parse(request("/items.json").body)
+      get :items
     end
 
     def self.details(item_id)
-      parse(request("/item_details.json", query: { item_id: item_id }).body)
+      get :item_details, item_id: item_id
     end
   end
 end
