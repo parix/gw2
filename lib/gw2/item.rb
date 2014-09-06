45af35fa02401b09af4e7a2cd0763df21f7833ea
Beau Dacious <dacious.beau@gmail.com>
2014-09-06 00:09:09 -0700

    consolidate Item methods

--- lib/gw2/item.rb
+++ lib/gw2/item.rb
@@ -1,9 +1,14 @@
-require "gw2/item/items"
-require "gw2/item/item_details"
-
 module GW2
   module Item
     extend HTTPS
     extend JSON
+
+    def self.all
+      parse(request("/items.json").body)
+    end
+
+    def self.details(item_id)
+      parse(request("/item_details.json", query: { item_id: item_id }).body)
+    end
   end
 end
--- spec/item/items_spec.rb
+++ spec/item/items_spec.rb
@@ -18,5 +18,34 @@ describe GW2::Item do
         GW2::Item.all.should == { "items" => @items }
       end
     end
+
+    context "#details" do
+      before :each do
+        @item_details = {
+          "item_id" => "12546",
+          "name" => "Lemongrass",
+          "description" => "Ingredient",
+          "type" => "CraftingMaterial",
+          "level" => "80",
+          "rarity" => "Basic",
+          "vendor_value" => "9",
+          "game_types" => ["Activity","Dungeon","Pve","Wvw"],
+          "flags" => [],
+          "restrictions" => [],
+          "crafting_material" => ""
+        }
+
+        stub_request(:get, "https://api.guildwars2.com/v1/item_details.json?item_id=12546").
+          to_return(:status => 200, :body => @item_details.to_json)
+      end
+
+      it "exists" do
+        GW2::Item.respond_to?(:details).should == true
+      end
+
+      it "returns the correct JSON parsed data" do
+        GW2::Item.details(12546).should == @item_details
+      end
+    end
   end
 end
