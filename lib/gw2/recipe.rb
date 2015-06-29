86d67dadb9deed5d86b87d4bc869e87025f38bb2
Allen Rosario <c.allen.rosario@gmail.com>
2015-06-28 20:55:22 -0700

    Updated recipe endpoint.

--- lib/gw2/recipe.rb
+++ lib/gw2/recipe.rb
@@ -2,12 +2,12 @@ module GW2
   module Recipe
     extend Resource
 
-    def self.all
-      get("/recipes.json")["recipes"]
+    def self.ids
+      get("/recipes")
     end
 
-    def self.details(recipe_id)
-      get("/recipe_details.json", { recipe_id: recipe_id })
+    def self.find(recipe_id)
+      get("/recipes/#{recipe_id}")
     end
   end
 end
--- spec/gw2/recipe_spec.rb
+++ spec/gw2/recipe_spec.rb
@@ -1,45 +1,45 @@
 require "spec_helper"
 
 describe GW2::Recipe do
-  describe ".all" do
+  describe ".ids" do
     before :each do
       @recipes = [1275, 1276, 1277, 1278, 1279]
     end
 
     it "exists" do
-      expect(GW2::Recipe.respond_to?(:all)).to eq(true)
+      expect(GW2::Recipe.respond_to?(:ids)).to eq(true)
     end
 
     it "returns all recipes", :vcr do
-      expect(GW2::Recipe.all).to include(*@recipes)
+      expect(GW2::Recipe.ids).to include(*@recipes)
     end
   end
 
-  describe ".details" do
+  describe ".find" do
     before :each do
       @recipe_details = {
-        "recipe_id" => "1275",
-        "type" => "Coat",
-        "output_item_id" => "11541",
-        "output_item_count" => "1",
-        "min_rating" => "25",
-        "time_to_craft_ms" => "1000",
-        "disciplines"=>["Leatherworker"],
-        "flags"=>[],
+        "type" => "RefinementEctoplasm",
+        "output_item_id" => 46742,
+        "output_item_count" => 1,
+        "min_rating" => 450,
+        "time_to_craft_ms" => 5000,
+        "disciplines" => [ "Armorsmith", "Artificer", "Huntsman", "Weaponsmith" ],
+        "flags" => [ "AutoLearned" ],
         "ingredients" => [
-          {"item_id" => "19797","count" => "1"},
-          {"item_id" => "13094","count" => "1"},
-          {"item_id" => "13093","count" => "1"}
-        ]
+          { "item_id" => 19684, "count" => 50 },
+          { "item_id" => 19721, "count" => 1 },
+          { "item_id" => 46747, "count" => 10 }
+        ],
+        "id" => 7319
       }
     end
 
     it "exists" do
-      expect(GW2::Recipe.respond_to?(:details)).to eq(true)
+      expect(GW2::Recipe.respond_to?(:find)).to eq(true)
     end
 
     it "returns the details of a recipe by id", :vcr do
-      expect(GW2::Recipe.details(1275)).to eq(@recipe_details)
+      expect(GW2::Recipe.find(7319)).to eq(@recipe_details)
     end
   end
 end
